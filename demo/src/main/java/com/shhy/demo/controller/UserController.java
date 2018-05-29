5d4b4358e15551cd377b3d46b1ed7250f50b4c9a
maochangan <738863752@qq.com>
2018-05-29 15:20:19 +0800

    demo

---
+++ demo/src/main/java/com/shhy/demo/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.shhy.demo.controller;
+
+import com.shhy.demo.bean.User;
+import com.shhy.demo.service.UserService;
+import com.shhy.demo.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "userController")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService ;
+
+    @RequestMapping(value = "testMvc", method = RequestMethod.GET)
+    public JsonResult testMvc(){
+
+        return JsonResult.success().add("msg", "msg");
+    }
+
+
+    @RequestMapping(value = "findAll", method = RequestMethod.GET)
+    public JsonResult findAll(){
+        List<User> list = userService.findAll();
+        if(null == list){
+            return JsonResult.fail().add("msg", "is null");
+        }else{
+            return JsonResult.success().add("list", list);
+        }
+    }
+
+    @RequestMapping(value = "getOneById" , method = RequestMethod.GET)
+    public JsonResult getOneById(Integer id){
+        User u = userService.getOneById(id);
+        if(null == u){
+            return JsonResult.fail().add("msg", "is null");
+        }else{
+            return JsonResult.success().add("u", u);
+        }
+    }
+
+    @RequestMapping(value = "getALlUser", method = RequestMethod.GET)
+    public JsonResult getALlUsers(){
+        List<User> list = userService.getAllUser();
+        if(null == list){
+            return JsonResult.fail().add("msg" , "is null");
+        }else{
+            return JsonResult.success().add("user", list);
+        }
+    }
+
+}
---
+++ demo/src/main/java/com/shhy/demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.shhy.demo.service;
+
+import com.shhy.demo.bean.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User getOneById(Integer id);
+
+    List<User> getAllUser();
+}
